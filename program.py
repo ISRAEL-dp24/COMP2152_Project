9838be81314f026a7354f88f4904f483ab6a6b6f
ISRAEL-dp24 <dolapoosunkoya5@gmail.com>
2025-04-06 17:49:24 -0400

    Implemented random encounter feature

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ import random
 
 # Put all the functions into another file and import them
 import function
+from encounters import trigger_random_encounter
 
 print("    ------------------------------------------------------------------")
 print("    |    Loading previous game data...")
@@ -139,6 +140,11 @@ if not input_invalid:
 
     # Use Loot
     belt, health_points = function.use_loot(belt, health_points)
+
+    print(" ----------------------------------------------------------------------------------------")
+    health_points, combat_strength, belt = trigger_random_encounter(health_points, combat_strength, belt)
+    print(f" |    Updated stats - Health: {health_points}, Combat: {combat_strength}, Belt: {belt}")
+
 
     print("    ------------------------------------------------------------------")
     print("    |", end="    ")
